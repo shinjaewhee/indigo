15547160409c821eae2df4801e480130614fed28
신재휘 <143384119+shinjaewhee@users.noreply.github.com>
2023-10-24 11:55:42 +0900

    Add files via upload

---
+++ js/default.js
@@ -0,0 +1,21 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // 옵션
+  // direction:'vertical',
+  loop:true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+function showNav(){
+  document.querySelector('#main-nav').classList.toggle('main-nav-active');
+}
